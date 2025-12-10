1202b0d71977559aa2b3fcb46f2752b68f8fc311
Brad Wilson <dotnetguy@gmail.com>
2025-12-10 11:30:31 -0800

    Remove extension block syntax (and extract ApiKey value validation for reuse)

--- src/Hyphen.Sdk/Extensions/HyphenSdkHttpClientExtensions.cs
+++ src/Hyphen.Sdk/Extensions/HyphenSdkHttpClientExtensions.cs
@@ -1,13 +1,7 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace System.Net.Http;
 
-[SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "Extension blocks are implemented as nested types")]
 internal static class HyphenSdkHttpClientExtensions
 {
-	extension(HttpClient client)
-	{
-		public void SetHyphenApiKey(string apiKey) =>
-			client.DefaultRequestHeaders.Add("x-api-key", apiKey);
-	}
+	public static void SetHyphenApiKey(this HttpClient client, string apiKey) =>
+		client.DefaultRequestHeaders.Add("x-api-key", apiKey);
 }
--- src/Hyphen.Sdk/Extensions/HyphenSdkServiceCollectionExtensions.cs
+++ src/Hyphen.Sdk/Extensions/HyphenSdkServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
-using System.Diagnostics.CodeAnalysis;
 using Hyphen.Sdk;
-using Hyphen.Sdk.Resources;
+using Hyphen.Sdk.Internal;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -8,59 +7,27 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// <summary>
 /// Extension methods for the Hyphen SDK.
 /// </summary>
-[SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "Extension blocks are implemented as nested types")]
 public static class HyphenSdkServiceCollectionExtensions
 {
-	extension(IServiceCollection services)
+	/// <summary>
+	/// Adds the <see cref="INetInfo"/> service to the <see cref="IServiceCollection"/>.
+	/// </summary>
+	/// <param name="services"/>
+	/// <returns>The <see cref="IServiceCollection"/>.</returns>
+	public static IServiceCollection AddNetInfo(this IServiceCollection services)
 	{
-		/// <summary>
-		/// Adds the <see cref="INetInfo"/> service to the <see cref="IServiceCollection"/>.
-		/// </summary>
-		/// <returns>The <see cref="IServiceCollection"/>.</returns>
-		public IServiceCollection AddNetInfo() =>
-			AddNetInfo(services, default!);
+		RegisterDependencies(Guard.ArgumentNotNull(services));
 
-		/// <summary>
-		/// Adds the <see cref="INetInfo"/> service to the <see cref="IServiceCollection"/>, allowing
-		/// the user to configure the service.
-		/// </summary>
-		/// <param name="configure">The action used to configure the options.</param>
-		/// <returns>The <see cref="IServiceCollection"/>.</returns>
-		public IServiceCollection AddNetInfo(Action<NetInfoOptions> configure)
-		{
-			RegisterDependencies(services);
+		services.TryAddSingleton<INetInfo, NetInfo>();
+		services
+			.AddOptionsWithValidateOnStart<NetInfoOptions>()
+			.Configure(options =>
+			{
+				options.BaseUri ??= new("https://net.info");
+			})
+			.ValidateApiKey();
 
-			services.TryAddSingleton<INetInfo, NetInfo>();
-			services
-				.AddOptionsWithValidateOnStart<NetInfoOptions>()
-				.Configure(options =>
-				{
-					configure?.Invoke(options);
-
-					options.BaseUri ??= new("https://net.info");
-				})
-				.Validate(
-					options =>
-					{
-						if (options.ApiKey is null)
-						{
-							if (Environment.GetEnvironmentVariable("HYPHEN_API_KEY") is string envApiKey)
-								options.ApiKey = envApiKey;
-							else
-								return false;
-						}
-
-						return true;
-					},
-					HyphenSdkResources.API_KEY_REQUIRED
-				)
-				.Validate(
-					options => !options.ApiKey.StartsWith("public_", StringComparison.InvariantCultureIgnoreCase),
-					HyphenSdkResources.PUBLIC_API_KEY_SHOULD_NOT_BE_USED
-				);
-
-			return services;
-		}
+		return services;
 	}
 
 	static void RegisterDependencies(IServiceCollection services)
