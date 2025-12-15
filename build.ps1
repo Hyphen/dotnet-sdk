0fc96e4a96ce65ec24e855d11702894824e7a144
Brad Wilson <dotnetguy@gmail.com>
2025-12-15 13:57:59 -0800

    chore: Generate a code coverage report for command line builds

--- build.ps1
+++ build.ps1
@@ -8,6 +8,12 @@ if ($null -eq (Get-Command "dotnet" -ErrorAction Ignore)) {
 	throw "Could not find 'dotnet'; please install the .NET SDK from https://dot.net/"
 }
 
+dotnet tool restore
 dotnet restore
 dotnet build --no-restore --configuration Release
-dotnet test --no-build --configuration Release
+dotnet test --no-build --configuration Release --coverage --coverage-output-format cobertura --coverage-output coverage.cobertura.xml
+dotnet reportgenerator -reports:**/TestResults/**/coverage.cobertura.xml -targetdir:CoverageReport -reporttypes:"TextSummary" -verbosity:off
+Write-Host ""
+Write-Host "Code coverage report:" -ForegroundColor DarkGreen
+Write-Host ""
+Get-Content CoverageReport/Summary.txt
