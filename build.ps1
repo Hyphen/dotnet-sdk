17f571d1f29e95c188a8494060269bde05e5f42a
Brad Wilson <dotnetguy@gmail.com>
2025-12-23 16:41:28 -0800

    chore: build.ps1 should exit if any command fails

--- build.ps1
+++ build.ps1
@@ -9,10 +9,20 @@ if ($null -eq (Get-Command "dotnet" -ErrorAction Ignore)) {
 }
 
 dotnet tool restore
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 dotnet restore
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 dotnet build --no-restore --configuration Release
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 dotnet test --no-build --configuration Release --coverage --coverage-output-format cobertura --coverage-output coverage.cobertura.xml --coverage-settings .runsettings
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 dotnet reportgenerator -reports:**/TestResults/**/coverage.cobertura.xml -targetdir:CoverageReport -reporttypes:"TextSummary" -verbosity:off
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 Write-Host ""
 Write-Host "Code coverage report:" -ForegroundColor DarkGreen
 Write-Host ""
